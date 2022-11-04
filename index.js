83d1cbfb680d26bad2347c5f711e269610e68d2a
Christopher Marinica <chris.marinica@gmail.com>
2022-11-04 12:52:22 -0400

    commit tree 8da09280cee64c54d20d2edb8cff214e8c376acf

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(a = 2000,b = 100) {
+    return (a / b)
+}
+
+const square = par1  => par1 * par1
+
+const add = (par1,par2) => {
+    console.log(`adding ${par1}`)
+    console.log(`adding ${par2}`)
+    return par1 + par2
+}
